df0df33d65334dc5d2fd3ea2f9bad3debb7dc259
Gregory Thompson <mrgregorythompsonii@gmail.com>
2021-02-01 13:51:57 -0600

    added login and register components; website redirects to login if 'runHub_user' is not found in localStorage

---
+++ src/components/RunHub.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { Route, Redirect } from "react-router-dom"
+import { ApplicationViews } from "./ApplicationViews"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
+
+export const RunHub = () => (
+    <>
+        <Route
+            render={() => {
+                if (localStorage.getItem("runHub_user")) {
+                    return (
+                        <>
+                            <ApplicationViews />
+                        </>
+                    )
+                } else {
+                    return <Redirect to="/login" />;
+                }
+            }}
+        />
+
+        <Route path="/login">
+            <Login />
+        </Route>
+        <Route path="/register">
+            <Register />
+        </Route>
+    </>
+)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom"
+import { RunHub } from './components/RunHub.js';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <RunHub /> 
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
